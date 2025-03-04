48ec6a90ab771a8acd7ac622748e0a6f3a48f4e9
naafidasana <ibrahimnaafi@gmail.com>
2025-03-04 16:47:25 +0100

    update path

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/portfolio/',
-  assetPrefix: '/portfolio/',
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
